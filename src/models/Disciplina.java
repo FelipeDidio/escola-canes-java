d6ef7ea8f7d9d610374c0d6f0fdc7c8cc7189c8d
Felipe Verdade <felipeverdade@gmail.com>
2023-10-06 21:45:29 -0300

    trabalho de hoje

--- src/models/Disciplina.java
+++ src/models/Disciplina.java
@@ -5,6 +5,9 @@
  */
 package models;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author 180701103
@@ -13,8 +16,8 @@ public class Disciplina {
     private int codigo;
     private String disciplina;
     
-    public Disciplina (int codigo, String disciplina){
-        
+    public Disciplina ( String disciplina){
+        this.disciplina = disciplina;
     }
 
     public int getCodigo() {
@@ -25,6 +28,7 @@ public class Disciplina {
         this.codigo = codigo;
     }
 
+    
     public String getDisciplina() {
         return disciplina;
     }
@@ -33,6 +37,40 @@ public class Disciplina {
         this.disciplina = disciplina;
     }
     
+    public void cadastrar(){
+        String sql = "INSERT INTO disciplina(disciplina) VALUES("
+                + " ' "+ this.getDisciplina()+ "' )";
+        System.out.println(sql);
+        Conexao.executar(sql);
+    }
+    public void editar(){
+        String sql = "UPDATE disciplina SET "
+                    + " ' " + this.getDisciplina() + " ' )";
+        Conexao.executar(sql);
+    }
     
+    public void excluir(String disciplina){
+        String sql = "DELETE FROM disciplina WHERE disciplina = " + disciplina;
+        Conexao.executar(sql);
+    }
+    
+    public static ArrayList<Disciplina> getDisciplinas(){
+     ArrayList<Disciplina> lista = new ArrayList<>();
+     String sql = "SELECT disciplina FROM ORDER BY disciplina";
+     ResultSet rs = Conexao.consultar(sql);
+     if(rs != null){
+         try{
+             while (rs.next()) {
+                 String disciplina = rs.getString(1);
+                 
+                 Disciplina discip = new Disciplina(disciplina);
+                 discip.setCodigo(rs.getInt("codigo"));
+                 lista.add(discip); 
+             }
+         }catch(Exception e){       
+         }
+     }
+      return lista;
+    }
     
 }
